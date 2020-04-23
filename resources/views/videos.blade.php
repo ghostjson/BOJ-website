99e6ecb003485e2bb96914945b619b50fd8cdbfa
Akhil Ravindran <imakhilravindran@gmail.com>
2020-04-23 16:44:44 +0530

    created video reward section

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -5,74 +5,112 @@
     <script src="https://kit.fontawesome.com/1b5e0bcd4a.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <style>
-        body{
-            margin: 0;
-            padding: 0;
+
+        h3{
+            width: 100%;
+            text-align: center;
+            font-family: Ubuntu, sans-serif;
+            font-size: 2em;
+            margin: 20px 0px;
         }
-        .vid-sec{
-            width: 80%;
-            height:auto;
-            background: white;
-            margin-left: 10%;
-            margin-top: 100px;
+
+        section.video{
+            width: 100%;
+        }
+
+        section.video div.video-platform{
+            width: 50%;
+            margin: 0 auto;
+        }
+
+        section.video .next{
+            width: 100%;
+            text-align: center;
+            margin-top: 20px;
 
         }
-        .video{
-            width: 60%;
-            height: 500px;
-            background: whitesmoke;
-            margin-bottom: 100px;
-            margin-left: 20%;
-        }
-        .title{
-            width: 80%;
-            height: 200px;
-            margin-left: 10%;
-            margin-top: 50px;
-            font-size: 30px;
-            color: #262626;
-            text-align: center;
-        }
-        .bottom-menu{
-            position: relative;
+
+        section.video .next a,section.video .next a:active,section.video .next a:visited,section.video .next a:hover{
+            font-size: 1.2em;
+            text-decoration: none;
+            background-color: #008ac9;
+            padding: 10px;
+            font-family: Ubuntu, sans-serif;
+            border-radius: 10px;
+            color: white;
         }
     </style>
+
+    <link href="https://vjs.zencdn.net/7.7.5/video-js.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-ads/6.6.5/videojs.ads.css">
 <body>
-
 @include('layout.navbar')
+<h3>Watach Today's videos</h3>
 
-<div class="title">
-    <p>Complete The Mission By Watching Maximum 4 Videos, any Video.<br>
-        Then You Will Be Done With Your First Mission.
-    <p style="font-size: 20px;margin-top: -20px;">While Watching Videos You Won't be Able To Rewind or Skip Any Part of The Video.<br>You have to watch full Video to Complete The Mission</p>
-    </p>
-</div>
-<div class="vid-sec">
-    <div class="video">
-        1
+
+<section class="video">
+    <div class="video-platform">
+        <video id="video-player" class="video-js vjs-default-skin vjs-big-play-centered">
+            <source src="ad.mp4">
+        </video>
+
     </div>
-    <div class="video">
-        2
+
+    <div class="next">
+        <a href="#">Next Video</a>
     </div>
-    <div class="video">
-        3
-    </div>
-    <div class="video">
-        4
-    </div>
-    <div class="video">
-        5
-    </div>
-    <div class="video">
-        6
-    </div>
-    <div class="video">
-        7
-    </div>
-    <div class="video">
-        8
-    </div>
-</div>
+</section>
+
+
+<script src="https://vjs.zencdn.net/7.7.5/video.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-ads/6.6.5/videojs.ads.min.js"></script>
+
+
+<script>
+    let videos_src = {
+        ad: '/assets/videos/ad.mp4',
+        src: '/assets/videos/main.mp4'
+    };
+
+    let played = 0;
+
+    const player = videojs('video-player', {
+        controls: true,
+        autoplay: false,
+        fluid: true
+    })
+
+    player.src(videos_src.ad);
+
+
+    player.on('play', ()=> {
+        if(played === 0){
+            player.controls(false)
+        }
+    })
+
+
+    player.on('ended', ()=> {
+
+
+        if(played === 0){
+            player.src(videos_src.src)
+            player.controls(true)
+            player.play()
+            played = 1;
+        }else{
+            fetch('/video/complete', {
+                method: 'GET'
+            });
+            alert('You are awarded $1.50')
+        }
+
+
+    })
+</script>
+
 
 
 <div class="bottom-menu">

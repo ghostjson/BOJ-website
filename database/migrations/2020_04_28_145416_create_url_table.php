fcf88f9e4137a509079c9684aac5529460a12289
Akhil Ravindran <imakhilravindran@gmail.com>
2020-04-29 21:27:29 +0530

    create url table

---
+++ database/migrations/2020_04_28_145416_create_url_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('urls');
+            $table->decimal('reward');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
